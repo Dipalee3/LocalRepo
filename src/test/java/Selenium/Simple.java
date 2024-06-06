95a5bca7dcc527eb39b9974080110fd6129dc337
Dipalee <dipaleepatil1@gmail.com>
2024-06-06 15:39:35 +0530

    commit

---
+++ src/test/java/Selenium/Simple.java
@@ -0,0 +1,22 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Simple {
+
+/*	System.setProperty("webdriver.chrome.driver","C:\\Github\\Driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver(); //upcasting
+	
+	driver.manage().window().maximize();
+	
+	driver.get("https://www.facebook.com");
+	
+	driver.quit();
+
+}*/
+}
+	
+
+
+
